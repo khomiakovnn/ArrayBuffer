c0539ed02cea4b92055e988dbf4c4f789c0396d2
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-09-20 17:46:20 -0400

    prepare

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,25 @@
+import app from '../app.js';
+
+// function typeTest() {
+//   return new Character('Ivan', 'Bowmann');
+// }
+// test('Incorrect type', () => {
+//   expect(typeTest).toThrow('Parameter "Type" is invalid!');
+// });
+
+// const typesDatalist = [
+//   'Bowman',
+//   'Swordsman',
+//   'Magician',
+//   'Daemon',
+//   'Undead',
+//   'Zombie',
+// ];
+
+// const typesHandler = test.each(typesDatalist);
+
+// typesHandler('Class Character for %s', (type) => {
+//   expect(new Character('Ivan', type)).toEqual({
+//     attack: undefined, defence: undefined, health: 100, level: 1, name: 'Ivan', type,
+//   });
+// });
